225948799e9fa8d74ac09accf5f3adecbd6d8e1d
linuxfight <kiko13024@gmail.com>
2023-08-12 12:30:07 +0300

    Fixed question

--- Day2_Middle/Program.cs
+++ Day2_Middle/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("What's your age?");
+        Console.WriteLine("How old are you?");
         var age = int.Parse(Console.ReadLine());
         Console.WriteLine($"Your age is {age}!");
     }
