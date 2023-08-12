016446ce12dd0d5f06b4434be157f7dec2d42df5
linuxfight <kiko13024@gmail.com>
2023-08-12 12:28:15 +0300

    Added Age Asking

--- Day2_Middle/Program.cs
+++ Day2_Middle/Program.cs
@@ -3,5 +3,8 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("What's your age?");
+        var age = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Your age is {age}!");
     }
 }
